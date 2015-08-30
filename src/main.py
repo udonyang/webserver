b6b1e69957bc3a5e13d2dbb6630cb02f4d223566
Dwylkz <xellosgeteternal@gmail.com>
2015-08-30 13:55:22 +0800

    add html

--- src/main.py
+++ src/main.py
@@ -2,18 +2,39 @@ import os
 import sys
 import BaseHTTPServer
 import cgi.loginpage
+import subprocess
+import jinja2 
+import urlparse
 
-def hearthstone_list():
-  pass
+tmpl_env = jinja2.Environment(loader=jinja2.PackageLoader('html', '.'))
+
+class HearthStoneHandler:
+  def __init__(self, qs):
+    self.qs = qs
+
+  def list(self):
+    return (tmpl_env.get_template('hearthstone/list.html').render(name='Dwylkz'), 200)
+
+  def add(self):
+    return (tmpl_env.get_template('hearthstone/add.html').render(name='Dwylkz'), 200)
 
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
   def do_GET(self):
-    index_file = open('html/index.html')
-    index = ''.join(index_file.readlines())
-    index_file.close()
-    self.log_message(self.path);
-    self.wfile.write(index)
-    self.send_response(200)
+    form = urlparse.urlparse(self.path)
+    qs = urlparse.parse_qs(form.query)
+    self.log_message(form.path)
+    self.log_message(repr(qs))
+
+    retcode = 404
+    if qs.has_key('action'):
+      output = 'page not found'
+      if 'list' in qs['action']:
+        (output, retcode) = HearthStoneHandler(qs).list()
+      elif 'add' in qs['action']:
+        (output, retcode) = HearthStoneHandler(qs).add()
+      self.wfile.write(output)
+
+    self.send_response(retcode)
     return 0
 
 def main(argv):
